a1cc94cf9224235eff4b8abd8d40813a63ff3593
David Soards <davidysoards@gmail.com>
2020-07-10 13:13:17 -0400

    update ACF

--- advanced-custom-fields.php
+++ advanced-custom-fields.php
@@ -127,7 +127,7 @@ if (function_exists('acf_add_local_field_group')) :
         array(
           'param' => 'post_type',
           'operator' => '==',
-          'value' => 'post',
+          'value' => 'events',
         ),
       ),
     ),
@@ -140,4 +140,53 @@ if (function_exists('acf_add_local_field_group')) :
     'active' => true,
     'description' => '',
   ));
+
+  acf_add_local_field_group(array(
+    'key' => 'group_5f07993637cd6',
+    'title' => 'Sponsors',
+    'fields' => array(
+      array(
+        'key' => 'field_5f07993f5eac5',
+        'label' => 'Image',
+        'name' => 'image',
+        'type' => 'image',
+        'instructions' => '',
+        'required' => 1,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'return_format' => 'array',
+        'preview_size' => 'medium',
+        'library' => 'all',
+        'min_width' => '',
+        'min_height' => '',
+        'min_size' => '',
+        'max_width' => '',
+        'max_height' => '',
+        'max_size' => '',
+        'mime_types' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'sponsors',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+  ));
+
 endif;
