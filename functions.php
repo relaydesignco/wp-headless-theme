a5be46141f605338c739eb01dd67e75206ec15d8
David Soards <davidysoards@gmail.com>
2020-09-11 21:22:18 -0400

    formatting

--- functions.php
+++ functions.php
@@ -15,13 +15,26 @@ add_theme_support('post-thumbnails');
 
 // function allow_svg($mimes)
 // {
-// $mimes['svg'] = 'image/svg+xml';
-// $mimes['svgz'] = 'image/svg+xml';
-// return $mimes;
+//     $mimes['svg'] = 'image/svg+xml';
+//     $mimes['svgz'] = 'image/svg+xml';
+//     return $mimes;
 // }
 // add_filter('upload_mimes', 'allow_svg');
 
 // function fix_mime_type_svg($data = null, $file = null, $filename = null, $mimes = null)
 // {
-// $ext = isset($data['ext']) ? $data['ext'] : '';
-// if (strlen($ext) < 1) { // $exploded=explode('.', $filename); // $ext=strtolower(end($exploded)); // } // if ($ext==='svg' ) { // $data['type']='image/svg+xml' ; // $data['ext']='svg' ; // } elseif ($ext==='svgz' ) { // $data['type']='image/svg+xml' ; // $data['ext']='svgz' ; // } // return $data; // } // add_filter('wp_check_filetype_and_ext', 'fix_mime_type_svg' , 75, 4);
+//     $ext = isset($data['ext']) ? $data['ext'] : '';
+//     if (strlen($ext) < 1) {
+//         $exploded=explode('.', $filename);
+//         $ext=strtolower(end($exploded));
+//     }
+//     if ($ext==='svg') {
+//         $data['type']='image/svg+xml' ;
+//         $data['ext']='svg' ;
+//     } elseif ($ext==='svgz') {
+//         $data['type']='image/svg+xml' ;
+//         $data['ext']='svgz' ;
+//     }
+//     return $data;
+// }
+// add_filter('wp_check_filetype_and_ext', 'fix_mime_type_svg', 75, 4);
